e38c82fcdcde927d9d53df103aa85dd3a4eca49d
Neulana <fupinyou@gmail.com>
2018-11-05 23:15:47 +0800

    ✨ add length-of-last-word solution

---
+++ string/length-of-last-word.py
@@ -0,0 +1,26 @@
+"""
+给定一个仅包含大小写字母和空格 ' ' 的字符串，返回其最后一个单词的长度。
+
+如果不存在最后一个单词，请返回 0 。
+
+说明：一个单词是指由字母组成，但不包含任何空格的字符串。
+
+示例:
+
+输入: "Hello World"
+输出: 5
+"""
+
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s) == 0:
+            return 0
+        s = s.split()
+        if len(s) > 0:
+            return len(s[-1])
+        return 0
