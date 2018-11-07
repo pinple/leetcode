e7e8b6e33a699917e446e9f333e7cc6be7bd66b0
Neulana <fupinyou@gmail.com>
2018-11-07 19:12:39 +0800

    add the rotate-array solution

---
+++ array/rotate-array.py
@@ -0,0 +1,45 @@
+"""
+给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+
+示例 1:
+
+输入: [1,2,3,4,5,6,7] 和 k = 3
+输出: [5,6,7,1,2,3,4]
+解释:
+向右旋转 1 步: [7,1,2,3,4,5,6]
+向右旋转 2 步: [6,7,1,2,3,4,5]
+向右旋转 3 步: [5,6,7,1,2,3,4]
+示例 2:
+
+输入: [-1,-100,3,99] 和 k = 2
+输出: [3,99,-1,-100]
+解释: 
+向右旋转 1 步: [99,-1,-100,3]
+向右旋转 2 步: [3,99,-1,-100]
+说明:
+
+尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+要求使用空间复杂度为 O(1) 的原地算法。
+"""
+
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) == 0 or k == 0:
+          return
+
+        def reverse(start, end, s):
+          while start < end:
+            s[start], s[end] = s[end], s[start]
+            start += 1
+            end -= 1
+
+        n = len(nums) - 1
+        k = k % len(nums)
+        reverse(0, n - k, nums)
+        reverse(n - k + 1, n, nums)
+        reverse(0, n, nums)
