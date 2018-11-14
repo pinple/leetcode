b4ed2f212e496badd8e07edb2564ccd6c3dc0b89
Neulana <fupinyou@gmail.com>
2018-11-14 21:17:35 +0800

    ✨ add remove-duplicates-from-sorted-list solution

---
+++ linkedlist/remove-duplicates-from-sorted-list.py
@@ -0,0 +1,35 @@
+"""
+给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+
+示例 1:
+
+输入: 1->1->2
+输出: 1->2
+示例 2:
+
+输入: 1->1->2->3->3
+输出: 1->2->3
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(None)
+        dummy.next = head
+        p = dummy
+        
+        while p and p.next:
+            if p.val == p.next.val:
+                p.next = p.next.next
+            else:
+                p = p.next
+        return dummy.next
