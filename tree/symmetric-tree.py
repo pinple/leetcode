c1b9254ae079cba002b9a4abc1c59a0c732d7981
Neulana <fupinyou@gmail.com>
2018-11-14 22:06:33 +0800

    ✨ add symmetric-tree solution

---
+++ tree/symmetric-tree.py
@@ -0,0 +1,42 @@
+"""
+给定一个二叉树，检查它是否是镜像对称的。
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+说明:
+
+如果你可以运用递归和迭代两种方法解决这个问题，会很加分。
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def helper(root, mirror):
+            if not root and not mirror:
+                return True
+            if root and mirror and root.val == mirror.val:
+                return helper(root.left, mirror.right) and helper(root.right, mirror.left)
+            return False
+        return helper(root, root)
