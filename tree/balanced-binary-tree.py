af5a6c8ce2d3cf23e64cfab9cfaad3fb7186c25d
Neulana <fupinyou@gmail.com>
2018-11-17 22:39:11 +0800

    ✨ add balanced-binary-tree solution

---
+++ tree/balanced-binary-tree.py
@@ -0,0 +1,59 @@
+"""
+给定一个二叉树，判断它是否是高度平衡的二叉树。
+
+本题中，一棵高度平衡二叉树定义为：
+
+一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+
+示例 1:
+
+给定二叉树 [3,9,20,null,null,15,7]
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回 true 。
+
+示例 2:
+
+给定二叉树 [1,2,2,3,3,null,null,4,4]
+
+       1
+      / \
+     2   2
+    / \
+   3   3
+  / \
+ 4   4
+返回 false 。
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def dfs(p):
+            if not p:
+                return 0
+            
+            left = dfs(p.left)
+            right = dfs(p.right)
+            if left == -1 or right == -1:
+                return -1
+            if abs(left - right) > 1:
+                return -1
+            return 1 + max(left, right)
+        if dfs(root) == -1:
+            return False
+        return True
