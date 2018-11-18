f81d765153d53def181e5bfc485e4bf76dcea9bf
Neulana <fupinyou@gmail.com>
2018-11-18 22:36:17 +0800

    ✨ add minimum-depth-of-binary-tree solution

---
+++ tree/minimum-depth-of-binary-tree.py
@@ -0,0 +1,44 @@
+"""
+给定一个二叉树，找出其最小深度。
+
+最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例:
+
+给定二叉树 [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最小深度  2.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        left = self.minDepth(root.left)
+        right = self.minDepth(root.right)
+        if not left and not right:
+            return 1
+        elif not left:
+            return right + 1
+        elif not right:
+            return left + 1
+        else:
+            return min(left, right) + 1
