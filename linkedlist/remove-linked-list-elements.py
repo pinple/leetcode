1b51ccc97f48500f857c79806e710c7779a073aa
付品友 <fupinyou@fupinyoudeMacBook-Pro.local>
2019-01-09 23:12:59 +0800

    ✨ add the remove-linked-list-elements solution

---
+++ linkedlist/remove-linked-list-elements.py
@@ -0,0 +1,31 @@
+"""
+删除链表中等于给定值 val 的所有节点。
+
+示例:
+
+输入: 1->2->6->3->4->5->6, val = 6
+输出: 1->2->3->4->5
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        dummy = ListNode(-1)
+        dummy.next = head
+        p = dummy
+        while p.next:
+          if p.next.val == val:
+            p.next = p.next.next
+          else:
+            p = p.next
+        return dummy.next
